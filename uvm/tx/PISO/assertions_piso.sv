59c50e576439c99ff382b813bb48cd1256ae4bb2
Tadros-wagih <133041939+Tadros-wagih@users.noreply.github.com>
2025-01-25 23:39:20 +0200

    Update assertions_piso.sv

--- uvm/tx/PISO/assertions_piso.sv
+++ uvm/tx/PISO/assertions_piso.sv
@@ -3,5 +3,10 @@ module assertions_piso (piso_if.DUT _if);
     //*****************************//
     // TODO: Write Assertions Here //
     //*****************************//
+    property reset;
+	@(posedge _if.clk) (!_if.rst_n) |=> (_if.serial_out==0) ;
+    endproperty
 
+    wr_ack_a:assert property (reset);
+    wr_ack_c:cover property (reset);
 endmodule
